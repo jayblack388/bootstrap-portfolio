447954d69c45472a9ab02b186cc5a44e2f88b205
jayblack388 <jblackwell072588@gmail.com>
2017-12-05 19:28:46 -0500

    updated project 3

--- assets/javascript/typemain.js
+++ assets/javascript/typemain.js
@@ -1,15 +1,33 @@
 $(document).ready(() => {
   $('#text').on('keyup', (event) => {
-    $('.preview').html($(event.currentTarget).val())
+    $('.preview').html($(event.currentTarget).val());
   });
   $('#font').on('change', (event) => {
-    $('.preview').css('font-family', $(event.currentTarget).val())
+    $('.preview').css('font-family', $(event.currentTarget).val());
   });
-  $('#weight').on('change', (event) => {
-    $('.preview').css('font-weight', $(event.currentTarget).val())
+  $("#weight").change(function(){
+    if($(this).is(":checked")){
+        $('.preview').addClass("bold"); 
+    }else{
+        $('.preview').removeClass("bold");  
+    }
+  });
+  $("#style").change(function(){
+    if($(this).is(":checked")){
+        $('.preview').addClass("italics"); 
+    }else{
+        $('.preview').removeClass("italics");  
+    }
+  });
+  $("#variant").change(function(){
+    if($(this).is(":checked")){
+        $('.preview').addClass("variant"); 
+    }else{
+        $('.preview').removeClass("variant");  
+    }
   });
   $('#size').on('keyup', (event) => {
     let fontSize = $(event.currentTarget).val() +'px';
     $('.preview').css('font-size', fontSize);
   });
-})
+});
